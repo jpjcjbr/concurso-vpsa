3f6f72910db7fa888eb492c494a97707236a579f
João Paulo <jpjcjbr@gmail.com>
2012-05-26 23:26:45 -0300

    correcoes lucao fase1

--- app/models/proposta.rb
+++ app/models/proposta.rb
@@ -1,3 +1,10 @@
 class Proposta < ActiveRecord::Base
   attr_accessible :descricao, :desenvolvedores, :email, :titulo, :objetivo, :plataforma
+  
+  validates_presence_of :descricao
+  validates_presence_of :desenvolvedores
+  validates_presence_of :email
+  validates_presence_of :titulo
+  validates_presence_of :objetivo
+  validates_presence_of :plataforma
 end
